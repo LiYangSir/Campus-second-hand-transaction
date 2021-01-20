0b750512af0e54b9d625d60afe5826952c9bf249
李扬 <7983072+LiYangSir@user.noreply.gitee.com>
2021-01-20 22:28:19 +0800

    项目结构修改

--- Campus-Transaction-product/src/main/java/com/quguai/campustransaction/product/CampusTransactionProductApplication.java
+++ Campus-Transaction-product/src/main/java/com/quguai/campustransaction/product/CampusTransactionProductApplication.java
@@ -1,13 +1,15 @@
 package com.quguai.campustransaction.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.quguai.campustransaction.product.dao")
 @SpringBootApplication
-public class CampusTransactionPruductApplication {
+public class CampusTransactionProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CampusTransactionPruductApplication.class, args);
+		SpringApplication.run(CampusTransactionProductApplication.class, args);
 	}
 
 }
--- Campus-Transaction-product/src/test/java/com/quguai/campustransaction/product/CampusTransactionPruductApplicationTests.java
+++ Campus-Transaction-product/src/test/java/com/quguai/campustransaction/product/CampusTransactionPruductApplicationTests.java
@@ -1,13 +1,26 @@
 package com.quguai.campustransaction.product;
 
+import com.quguai.campustransaction.product.entity.BrandEntity;
+import com.quguai.campustransaction.product.service.BrandService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class CampusTransactionPruductApplicationTests {
 
+	@Autowired
+	private BrandService service;
+
 	@Test
 	void contextLoads() {
+		BrandEntity brandEntity = new BrandEntity();
+		brandEntity.setDescript("");
+		brandEntity.setName("huawei");
+		service.save(brandEntity);
+		log.info("保存成功");
 	}
 
 }
