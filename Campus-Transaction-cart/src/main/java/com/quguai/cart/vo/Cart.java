8f622e346619064c66010818a1a7ae66648c8d80
李扬 <7983072+LiYangSir@user.noreply.gitee.com>
2021-02-24 16:25:55 +0800

    购物车服务

---
+++ Campus-Transaction-cart/src/main/java/com/quguai/cart/vo/Cart.java
@@ -0,0 +1,54 @@
+package com.quguai.cart.vo;
+
+import org.thymeleaf.util.ListUtils;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Cart {
+    private List<CartItem> items;
+    private Integer countNum;
+    private Integer countType;
+    private BigDecimal totalAmount;
+    private BigDecimal reduce = new BigDecimal("0.00");
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
+
+    public Integer getCount() {
+        int count = 0;
+        if (!ListUtils.isEmpty(items)) {
+            for (CartItem item : items) {
+                count += item.getCount();
+            }
+        }
+        return count;
+    }
+
+    public Integer getCountType() {
+        return ListUtils.isEmpty(items)? 0: items.size();
+    }
+
+    public BigDecimal getTotalAmount() {
+        BigDecimal amount = new BigDecimal(0);
+        if (!ListUtils.isEmpty(items)) {
+            for (CartItem item : items) {
+                amount = amount.add(item.getTotalPrice());
+            }
+        }
+        return amount.subtract(getReduce());
+    }
+
+    public BigDecimal getReduce() {
+        return reduce;
+    }
+
+    public void setReduce(BigDecimal reduce) {
+        this.reduce = reduce;
+    }
+}
---
+++ Campus-Transaction-cart/src/test/java/com/quguai/cart/CampusCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quguai.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
